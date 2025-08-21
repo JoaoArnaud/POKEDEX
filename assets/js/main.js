2bb60ab3478f2e08ffa9bd546fca1c51281e7f8c
João André de Medeiros Arnaud <joaoandrema@gmail.com>
2025-08-21 20:44:12 -0300

    organizando código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToLI(pokemon){
     return `
     <li class="pokemon">
@@ -22,13 +18,6 @@ function convertPokemonToLI(pokemon){
 
 const pokemonList = document.getElementById("pokemonList");
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i<pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLI(pokemon)
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLI).join("");
+})
