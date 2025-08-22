8eb20d9589ff870650decbef1be8d88614ef0692
João André de Medeiros Arnaud <joaoandrema@gmail.com>
2025-08-21 21:52:02 -0300

    adicionando os tipos do pokemon dinamicamente

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,8 +1,27 @@
 const pokeApi = {}
 
-pokeApi.getPokemonsDetail = (pokemon) => fetch(pokemon.url).then((response) => response.json())
+function convertPokeAPIDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.order;
+    pokemon.name = pokeDetail.name;
 
-pokeApi.getPokemons = (offset=0, limit=10) => {
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
+
+    pokemon.types = types;
+    pokemon.type = type;
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon;
+}
+
+pokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeAPIDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset=0, limit=100) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
